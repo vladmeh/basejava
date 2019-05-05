48589bbb2a7be86a4bb091f35e9a6fc341b78e8d
vladmeh <vladmeh@gmail.com>
2019-05-05 20:45:22 +0300

    invoke_toString_via_reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,6 +3,8 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author Vladimir Mikhaylov <vladmeh@gmail.com> on 05.05.2019.
@@ -10,13 +12,17 @@ import java.lang.reflect.Field;
  */
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
+
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+
+        Method method = r.getClass().getMethod("toString");
+        System.out.println(method.invoke(r));
         // TODO : invoke r.toString via reflection
         System.out.println(r);
     }
